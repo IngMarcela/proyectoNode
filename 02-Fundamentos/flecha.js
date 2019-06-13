767a49868eb75f90e2f3154dfb8b0dc194b2eda2
Marcela Dominguez <marceladominguez1993@gmail.com>
2019-06-12 22:44:25 -0500

    funcion flecha en node

---
+++ 02-Fundamentos/flecha.js
@@ -0,0 +1,14 @@
+// function sumar(a, b) {
+//     return a + b;
+// }
+
+// let sumar = (a, b) => a + b;
+
+// function saludar() {
+//     return "Hola mundo";
+// }
+
+// let saludar = () => "hola mundo";
+
+let saludar = (nombre) => `hola ${ nombre }`
+console.log(saludar(`abec`));
