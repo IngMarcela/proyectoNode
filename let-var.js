dd364176fee28103dd7640323d4536ab7524f7e9
Marcela Dominguez <marceladominguez1993@gmail.com>
2019-06-12 22:24:25 -0500

    diferencia de let o var

---
+++ let-var.js
@@ -0,0 +1,18 @@
+/*let nombre = 'wolverine';
+
+if (truelet) {
+    let nombre = 'Magneto';
+}
+
+let nombre = 'wolverine1';
+let nombre = 'wolverine2';
+let nombre = 'wolverine3';
+let nombre = 'wolverine4';
+
+console.log(nombre);*/
+let i;
+for (let i = 0; i <= 5; i++) {
+    console.log(`i: ${ i }`);
+}
+
+console.log(i);
